cb38819a847e2733e81faed0d72a6166bd193eae
Barberousssee <yohan.rajotte@hotmail.com>
2024-04-18 16:34:54 -0400

    Ajout de mon petit formulaire québécois

--- MonPetitFormulaireQuebecois/MonPetitFormulaireQuebecois/Form1.Designer.cs
+++ MonPetitFormulaireQuebecois/MonPetitFormulaireQuebecois/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(189, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(437, 189);
+            button1.TabIndex = 0;
+            button1.Text = "Vive le Québec LIBRE";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
